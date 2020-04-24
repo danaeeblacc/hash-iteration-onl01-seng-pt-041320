2f86dcc98544c0c683fa8f2656be6dd1687fd2c4
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-24 23:45:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,7 +17,7 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts “you are too old for this.”
-  end
+    end
 end
 
  
